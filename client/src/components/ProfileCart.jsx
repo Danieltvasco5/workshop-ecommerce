3968d76355ddcbdd5ab92d70cf953e1a595198ba
Andres vega <jandresvega@itsa.edu.co>
2022-12-21 20:28:14 -0500

    Actualizando repo

---
+++ client/src/components/ProfileCart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/ProfileCart.css'
+import cart from '../img/icon-cart.svg';
+import procfile from '../img/image-avatar.png'
+const ProfileCart = () => {
+    return (
+        <div className="container-profile">
+            <img className="icon-cart" src={cart} alt="icon-cart" />
+            <img className="user-profile" src={procfile} alt="icon-procfile"/>
+        </div>
+    );
+};
+
+export default ProfileCart;
